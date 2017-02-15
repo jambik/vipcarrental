2652139b9e9c29681e588739c6f4eae914d0d1ed
jambik <jambik@gmail.com>
2017-02-16 00:00:48 +0300

    - commit

--- js/locations-autocomplete.js
+++ js/locations-autocomplete.js
@@ -1,9 +1,4 @@
 var locations = [
-{ value: "Santa Monica - 2102 Lincoln Blvd"},
-{ value: "With Co-Ordinates  - 5711 W Century Blvd", latLng: [33.946272, -118.381641]},
-{ value: "Las Vegas - 6401 Centennial Center Blvd"},
-{ value: "New York - 610 Warren St"},
-{ value: "Orlando - 2125 W Landstreet Rd"},
-{ value: "Washington - 50 Massachusetts Ave NE"},
-{ value: "Denver - 7800 E Tufts Ave"}
+    { value: "UAE, Dubai, Sheikh Zayed Rd, Al Attar Tower"},
+    { value: "England, London, W1J 6BR, Berkeley Square, Berkeley Square House"},
 ];
